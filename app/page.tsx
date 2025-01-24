ec707693c7a526cf44d9a74e98f1cf90efbef77c
cperlovan <cperlovan@gmail.com>
2025-01-24 15:31:10 -0400

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "../../app/globals.css"
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../app/globals.css'
 import Header from "./Components/Header";
+
 export default function Home() {
   return (
     <>
     <Header/>
     <h1>PROJECT BUILD</h1>
-    <script
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-                crossOrigin="anonymous"
-            ></script>
+
     </>
   );
 }
