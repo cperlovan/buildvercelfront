ad51edbc759cfa77e1c37c0bdf159aca42b924d5
cperlovan <cperlovan@gmail.com>
2025-01-24 10:17:02 -0400

    import bootstrap cdn en page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "../../app/globals.css"
 
 import Header from "./Components/Header";
 export default function Home() {
@@ -6,6 +7,11 @@ export default function Home() {
     <>
     <Header/>
     <h1>PROJECT BUILD</h1>
+    <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+                crossOrigin="anonymous"
+            ></script>
     </>
   );
 }
