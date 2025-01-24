e07e382992106263f0b7af920b3994cb450d4583
cperlovan <cperlovan@gmail.com>
2025-01-24 10:11:01 -0400

    import bootstrap cdn

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import "../../app/globals.css"
+import "../../app/globals.css"
 // import "../../public/assets/css/style.css"
 
 import React from 'react'
@@ -56,13 +56,18 @@ function Header() {
                                 <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
-                       {/*  <form className="d-flex">
+                        {/*  <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form> */}
                     </div>
                 </div>
             </nav>
+            <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+                crossOrigin="anonymous"
+            ></script>
         </>
     )
 }
