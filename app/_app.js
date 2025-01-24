6c7ca5e685754f805e3b326d3e478ad8d7498d08
cperlovan <cperlovan@gmail.com>
2025-01-24 16:56:39 -0400

    _app bootstrap

--- app/_app.js
+++ app/_app.js
@@ -5,6 +5,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <script
+        strategy="afterInteractive"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0g1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossOrigin="anonymous"
+      />
     </>
   );
 }
