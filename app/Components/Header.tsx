34c701f61486c337684809e1edf9d4d88e2ecd70
cperlovan <cperlovan@gmail.com>
2025-01-25 17:16:26 -0400

    header tailwind

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,71 +1,40 @@
 "use client";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "../../app/globals.css"
-
-// import "../../public/assets/css/style.css"
-
-import React from 'react'
-
-
+import "../../app/globals.css";
+// import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function Header() {
-
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Construction Monitoring</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/home">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/jobcreate">Job create</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/billcreate">Bill create</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/pocreate">PO create</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/jobquery">Job query</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/poquery">P.O. query</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/billquery">Bill query</a>
-                            </li>
-                            {/* <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li> */}
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                        {/*  <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form> */}
-                    </div>
-                </div>
-            </nav>
-            
-        </>
-    )
+  return (
+    <>
+      <nav className="bg-blue-500 text-white p-4"> 
+        <div className="container mx-auto flex justify-between items-center"> 
+          <h1 className="text-2xl font-bold">Construction Monitoring</h1>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/home" className="text-white hover:underline">Home</a>
+            </li>
+            <li>
+              <a href="/jobcreate" className="text-white hover:underline">Job create</a>
+            </li>
+            <li>
+              <a href="/billcreate" className="text-white hover:underline">Bill create</a>
+            </li>
+            <li>
+              <a href="/pocreate" className="text-white hover:underline">PO create</a>
+            </li>
+            <li>
+              <a href="/jobquery" className="text-white hover:underline">Job query</a>
+            </li>
+            <li>
+              <a href="/poquery" className="text-white hover:underline">P.O. query</a>
+            </li>
+            <li>
+              <a href="/billquery" className="text-white hover:underline">Bill query</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
