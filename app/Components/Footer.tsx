8be603999a5719eef03d49d1627a7f1342fb2416
cperlovan <cperlovan@gmail.com>
2025-01-26 15:41:34 -0400

    footer mod

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
 <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https:#" className="hover:underline">VCCP Technologies</a>. <strong>Construction Monitoring</strong> All Rights Reserved.
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><strong>Construction Monitoring </strong> © 2024 <a href="https:#" className="hover:underline">VCCP Technologies</a>.  All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
