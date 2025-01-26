d039f295c4da569dbebbd8753df8d915efaa3ae5
cperlovan <cperlovan@gmail.com>
2025-01-26 14:20:39 -0400

    billcreate style

--- app/billcreate/page.tsx
+++ app/billcreate/page.tsx
@@ -27,7 +27,7 @@ interface Jobsexcel {
   Comments: string;
   VarianceCodes: string;
   CostCodes: string;
-  RelatedPOs:	string;
+  RelatedPOs: string;
   LienWaivers: string;
 
 }
@@ -37,20 +37,20 @@ function Page() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
- 
+
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-  
+
     if (!file) return; // Handle case where no file is selected
-  
+
     setIsLoading(true);
     setError(""); // Clear any previous errors
-  
+
     const reader = new FileReader();
     reader.onload = (e) => {
       const data = e.target?.result;
-  
+
       try {
         const workbook = XLSX.read(data, { type: 'binary', cellText: false, cellDates: true });
         const sheetName = workbook.SheetNames[0];
@@ -64,18 +64,18 @@ function Page() {
         setIsLoading(false);
       }
     };
-  
+
     reader.readAsBinaryString(file);
   };
 
-   // Function to format date in mm/dd/yyyy
+  // Function to format date in mm/dd/yyyy
   //  const formatDate = (date: Date) => {
   //   if (!date) return ''; 
-  
+
   //   const month = String(date.getMonth() + 1).padStart(2, '0'); 
   //   const day = String(date.getDate()).padStart(2, '0'); 
   //   const year = date.getFullYear(); 
-  
+
   //   return `${month}/${day}/${year}`; 
   // };
 
@@ -87,11 +87,11 @@ function Page() {
       // You can also display an error message in the UI using a state variable
     }
   }, [error]);
-  
- 
+
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-  
+
     // Check if there's data to send
     if (!data || data.length === 0) {
       console.error('No data to submit. Please upload an Excel file.');
@@ -134,24 +134,24 @@ function Page() {
       ...row,
       InvoiceDate: moment(row.InvoiceDate, 'MM/DD/YYYY', true).isValid() ?
         moment(row.InvoiceDate, 'MM/DD/YYYY').format('YYYY-MM-DD') : null,
-        DueDate: moment(row.DueDate, 'MM/DD/YYYY', true).isValid() ?
+      DueDate: moment(row.DueDate, 'MM/DD/YYYY', true).isValid() ?
         moment(row.DueDate, 'MM/DD/YYYY').format('YYYY-MM-DD') : null,
-        DatePaid: moment(row.DatePaid, 'MM/DD/YYYY', true).isValid() ?
+      DatePaid: moment(row.DatePaid, 'MM/DD/YYYY', true).isValid() ?
         moment(row.DatePaid, 'MM/DD/YYYY').format('YYYY-MM-DD') : null,
-        CreatedDate: moment(row.CreatedDate, 'MM/DD/YYYY', true).isValid() ?
+      CreatedDate: moment(row.CreatedDate, 'MM/DD/YYYY', true).isValid() ?
         moment(row.CreatedDate, 'MM/DD/YYYY').format('YYYY-MM-DD') : null,
     }));
 
 
-    
+
     console.log(formattedData)
-   
-  
+
+
     // Prepare the data to send (assuming your backend expects JSON)
     const jobsData = JSON.stringify(formattedData);
 
-    
-     
+
+
     // Send the data to your backend using fetch or any other HTTP library
     deleteTableData()
     fetch('https://constructapi.vercel.app/bills', {
@@ -179,7 +179,7 @@ function Page() {
         // Handle network errors (optional)
         setError('An error occurred while communicating with the server. Please check your internet connection and try again.');
       });
-      
+
   }
 
   async function deleteTableData() {
@@ -187,89 +187,89 @@ function Page() {
       const response = await fetch('https://constructapi.vercel.app/bills/delete', {
         method: 'PUT',
       });
-  
+
       if (!response.ok) {
         throw new Error('Error deleting table');
       }
-  
+
       console.log('Table deleted successfully');
     } catch (error) {
       console.error('Error deleting table:', error);
       // Manejar el error, por ejemplo, mostrar un mensaje al usuario
     }
   }
-    
+
   return (
-    <div>
-      <div className='w-full mx-auto px-4'>
+    <div className='w-full mx-auto px-4'>
       <Header />
-      </div>
-      <div className="table-responsive">
-      <div className='mb-3 ml-4' >
-      <label htmlFor="formFile" className="form-label">Choose file</label> 
-        <input
-          className='form-control' 
-          type="file" 
-          id="formFile"
-          onChange={handleFileChange} 
-          style={{ display: 'block', fontSize: 'small' }} 
-        />
-        </div>
-      {isLoading && <p>Loading data...</p>}
-      {error && <p className="error">{error}</p>}
-      <form className="form-control" onSubmit={handleSubmit} >
-        <button className="btn btn-secondary"  style={{fontSize: 'small', marginLeft:'5px'}} >Save bills to database</button>
-      {!isLoading && data.length > 0 && (
-        <table id="Datatable" className="table table-hover fs-6 table-striped mt-3">
-          <thead>
-            <tr>
-              <th>Job</th>
-              <th>Bill</th>
-              <th>Bill Title</th>
-              <th>Pay to</th>
-              <th>Bill amount</th>
-              <th>Invoce date</th>
-              <th>Due date</th>
-              <th>Bill status</th>
-              <th>Date Paid</th>
-              <th>Paid by</th>
-              <th>Created date</th>
-              <th>Files</th>
-              <th>Comments</th>
-              <th>Variance codes</th>
-              <th>Cost codes</th>
-              <th>Related POs</th>
-              <th>Lien Waivers</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
-                <td>{row.Job}</td>
-              <td>{row.Bill}</td>
-              <td>{row.BillTitle}</td>
-              <td>{row.PayTo}</td>
-              <td>{row.BillAmount}</td>
-              <td>{row.InvoiceDate ? new Date(row.InvoiceDate).toLocaleDateString('en-US',) : ''}</td>
-              <td>{row.DueDate ? new Date(row.DueDate).toLocaleDateString('en-US',) : ''}</td>
-              <td>{row.BillStatus}</td>
-              <td>{row.DatePaid ? new Date(row.DatePaid).toLocaleDateString('en-US',) : ''}</td>
-              <td>{row.PaidBy}</td>
-              <td>{row.CreatedDate ? new Date(row.CreatedDate).toLocaleDateString('en-US',) : ''}</td>
-              <td>{row.Files}</td>
-              <td>{row.Comments}</td>
-              <td>{row.VarianceCodes}</td>
-              <td>{row.CostCodes}</td>
-              <td>{row.RelatedPOs}</td>
-              <td>{row.LienWaivers}</td>
-                </tr>
-              ))}
+      <div>
+      <div className="table-responsive rounded-sm bg-gray-2">
+          <div className='mb-3 ' >
+          <label className="mb-3 block text-sm font-bold text-black dark:text-white ml-2"> Attach file</label>
+            <input
+              className='w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary'
+              type="file"
+              id="formFile"
+              onChange={handleFileChange}
+              style={{ display: 'block', fontSize: 'small' }}
+            />
+          </div>
+          {isLoading && <p>Loading data...</p>}
+          {error && <p className="error">{error}</p>}
+          <form className="bg-gray-100 p-4 rounded-md shadow-sm mt-3" onSubmit={handleSubmit} >
+            <button className="flex w-full items-center text-white justify-start gap-3.5 rounded-lg border border-stroke bg-gray-800 p-4 hover:bg-gray-900 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50 " disabled={isLoading} style={{ fontSize: 'small', marginLeft: '5px' }} >Save bills to database</button>
+            {!isLoading && data.length > 0 && (
+              <table id="Datatable" className="table table-hover fs-6 table-striped mt-3">
+                <thead>
+                  <tr>
+                    <th>Job</th>
+                    <th>Bill</th>
+                    <th>Bill Title</th>
+                    <th>Pay to</th>
+                    <th>Bill amount</th>
+                    <th>Invoce date</th>
+                    <th>Due date</th>
+                    <th>Bill status</th>
+                    <th>Date Paid</th>
+                    <th>Paid by</th>
+                    <th>Created date</th>
+                    <th>Files</th>
+                    <th>Comments</th>
+                    <th>Variance codes</th>
+                    <th>Cost codes</th>
+                    <th>Related POs</th>
+                    <th>Lien Waivers</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((row, index) => (
+                    <tr key={index}>
+                      <td>{row.Job}</td>
+                      <td>{row.Bill}</td>
+                      <td>{row.BillTitle}</td>
+                      <td>{row.PayTo}</td>
+                      <td>{row.BillAmount}</td>
+                      <td>{row.InvoiceDate ? new Date(row.InvoiceDate).toLocaleDateString('en-US',) : ''}</td>
+                      <td>{row.DueDate ? new Date(row.DueDate).toLocaleDateString('en-US',) : ''}</td>
+                      <td>{row.BillStatus}</td>
+                      <td>{row.DatePaid ? new Date(row.DatePaid).toLocaleDateString('en-US',) : ''}</td>
+                      <td>{row.PaidBy}</td>
+                      <td>{row.CreatedDate ? new Date(row.CreatedDate).toLocaleDateString('en-US',) : ''}</td>
+                      <td>{row.Files}</td>
+                      <td>{row.Comments}</td>
+                      <td>{row.VarianceCodes}</td>
+                      <td>{row.CostCodes}</td>
+                      <td>{row.RelatedPOs}</td>
+                      <td>{row.LienWaivers}</td>
+                    </tr>
+                  ))}
                 </tbody>
-        </table>
-      )}
-      </form>
+              </table>
+            )}
+          </form>
+        </div>
+
       </div>
-      
     </div>
   );
 }
