d0d8f0f7f8e57256520b98e2b8be2beed5b4dbd7
cperlovan <cperlovan@gmail.com>
2025-01-26 13:13:07 -0400

    pocreate input

--- app/pocreate/page.tsx
+++ app/pocreate/page.tsx
@@ -151,9 +151,9 @@ function Page() {
       <div>
         <div className="table-responsive">
           <div className='mb-3 ml-4' >
-            <label htmlFor="formFile" className="form-label">Choose file</label>
+          <label className="mb-3 block text-sm font-medium text-black dark:text-white">Attach file</label>
             <input
-              className='form-control'
+              className='w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary'
               type="file"
               id="formFile"
               onChange={handleFileChange}
