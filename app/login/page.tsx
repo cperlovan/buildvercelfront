d23c61e39d0a15cf55ab0417be1d4a5f5d920772
cperlovan <cperlovan@gmail.com>
2025-02-10 15:18:14 -0400

    endpoint https

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:3030/auth/login", {
+      const response = await fetch("https://constructapi.vercel.app/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password, role }),
