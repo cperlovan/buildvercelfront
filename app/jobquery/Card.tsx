4d5b2c9b0d88d4b6c4c366f164de1424a69a700e
cperlovan <cperlovan@gmail.com>
2025-02-18 19:30:06 -0400

    ready for payment

--- app/jobquery/Card.tsx
+++ app/jobquery/Card.tsx
@@ -287,7 +287,7 @@ export default function Card({ selectedJob }: { selectedJob: Jobsexcel | null })
                                                     <tr key={vendor} onClick={() => handlePayToClick({ PayTo: vendor , Job: selectedJob?.Name || '' })}>
                                                         <td>{vendor}</td>
                                                         <td>{formatNumber(amounts[Object.keys(amounts)[0]])}</td>
-                                                        <td>Ready for paid</td>
+                                                        <td>Ready For Payment</td>
                                                     </tr>
                                                 ))}
                                             <tr>
